61c9e8c3db946dbae9b17cf35b3bdf48e85049ea
Radu Gheorghiu <radu.gheorghiu@yahoo.com>
2017-08-22 17:03:55 +0300

    Update check_plan_cache.sql
    Fix for alignment of aliases in file (no functional change)

--- check_plan_cache.sql
+++ check_plan_cache.sql
@@ -1,50 +1,50 @@
 create function tvf_StoredProcedureData (@procedure_name nvarchar(max))
 	returns @result table
 	(
-		[Object Name]														[nvarchar](128)			NULL,
-		[Object ID]															[int]					NULL,
-		[Procedure time]													[numeric](38, 6)		NULL,
-		[rank-execution time]												[bigint]				NULL,
-		[rank-logical reads]												[bigint]				NULL,
-		[rank-worker (CPU) time]											[bigint]				NULL,
-		[rank-logical write]												[bigint]				NULL,
-		[SQL_Handle]														[varbinary](64)			NOT NULL,
-		[Statement_Start_Offset]											[int]					NOT NULL,
-		[Statement_End_Offset]												[int]					NOT NULL,
-		[Plan_Generation_Num]												[bigint]				NOT NULL,
-		[Plan_Handle]														[varbinary](64)			NOT NULL,
-		[Creation_Time]														[datetime]				NOT NULL,
-		[Last_Execution_Time]												[datetime]				NOT NULL,
-		[Execution_Count]													[bigint]				NOT NULL,
-		[Total_Worker_Time - CPU]											[numeric](18, 6)		NULL,
-		[Last_Worker_Time - CPU]											[numeric](18, 6)		NULL,
-		[Min_Worker_Time - CPU]												[numeric](18, 6)		NULL,
-		[Max_Worker_Time - CPU]												[numeric](18, 6)		NULL,
-		[Total_Physical_Reads - Disk pages read]							[bigint]				NOT NULL,
-		[Last_Physical_Reads - Disk pages read]								[bigint]				NOT NULL,
-		[Min_Physical_Reads - Disk pages read]								[bigint]				NOT NULL,
-		[Max_Physical_Reads - Disk pages read]								[bigint]				NOT NULL,
-		[Total_Logical_Writes - Memory (buffer cache) pages written]		[bigint]				NOT NULL,
-		[Last_Logical_Writes - Memory pages written]						[bigint]				NOT NULL,
-		[Min_Logical_Writes - Memory pages written]							[bigint]				NOT NULL,
-		[Max_Logical_Writes - Memory pages written]							[bigint]				NOT NULL,
-		[Total_Logical_Reads - Memory (buffer cache) pages read]			[bigint]				NOT NULL,
-		[Last_Logical_Reads - Memory pages read]							[bigint]				NOT NULL,
-		[Min_Logical_Reads - Memory pages read]								[bigint]				NOT NULL,
-		[Max_Logical_Reads - Memory pages read]								[bigint]				NOT NULL,
-		[Total_Elapsed_Time]												[numeric](18, 6)		NULL,
-		[Last_Elapsed_Time]													[numeric](18, 6)		NULL,
-		[Min_Elapsed_Time]													[numeric](18, 6)		NULL,
-		[Max_Elapsed_Time]													[numeric](18, 6)		NULL,
-		[Query_Hash]														[binary](8)				NOT NULL,
-		[Query_Plan_Hash]													[binary](8)				NOT NULL,
-		[Total_Rows - in all executions]									[bigint]				NOT NULL,
-		[Last_Rows]															[bigint]				NOT NULL,
-		[Min_Rows]															[bigint]				NOT NULL,
-		[Max_Rows]															[bigint]				NOT NULL,
-		[Filtered text]														[nvarchar](max)			NULL,
-		[Temp table]														[nvarchar](max)			NULL,
-		[Execution plan]													[xml]					NULL		
+		[Object Name]								[nvarchar](128)			NULL,
+		[Object ID]								[int]				NULL,
+		[Procedure time]							[numeric](38, 6)		NULL,
+		[rank-execution time]							[bigint]			NULL,
+		[rank-logical reads]							[bigint]			NULL,
+		[rank-worker (CPU) time]						[bigint]			NULL,
+		[rank-logical write]							[bigint]			NULL,
+		[SQL_Handle]								[varbinary](64)			NOT NULL,
+		[Statement_Start_Offset]						[int]				NOT NULL,
+		[Statement_End_Offset]							[int]				NOT NULL,
+		[Plan_Generation_Num]							[bigint]			NOT NULL,
+		[Plan_Handle]								[varbinary](64)			NOT NULL,
+		[Creation_Time]								[datetime]			NOT NULL,
+		[Last_Execution_Time]							[datetime]			NOT NULL,
+		[Execution_Count]							[bigint]			NOT NULL,
+		[Total_Worker_Time - CPU]						[numeric](18, 6)		NULL,
+		[Last_Worker_Time - CPU]						[numeric](18, 6)		NULL,
+		[Min_Worker_Time - CPU]							[numeric](18, 6)		NULL,
+		[Max_Worker_Time - CPU]							[numeric](18, 6)		NULL,
+		[Total_Physical_Reads - Disk pages read]				[bigint]			NOT NULL,
+		[Last_Physical_Reads - Disk pages read]					[bigint]			NOT NULL,
+		[Min_Physical_Reads - Disk pages read]					[bigint]			NOT NULL,
+		[Max_Physical_Reads - Disk pages read]					[bigint]			NOT NULL,
+		[Total_Logical_Writes - Memory (buffer cache) pages written]		[bigint]			NOT NULL,
+		[Last_Logical_Writes - Memory pages written]				[bigint]			NOT NULL,
+		[Min_Logical_Writes - Memory pages written]				[bigint]			NOT NULL,
+		[Max_Logical_Writes - Memory pages written]				[bigint]			NOT NULL,
+		[Total_Logical_Reads - Memory (buffer cache) pages read]		[bigint]			NOT NULL,
+		[Last_Logical_Reads - Memory pages read]				[bigint]			NOT NULL,
+		[Min_Logical_Reads - Memory pages read]					[bigint]			NOT NULL,
+		[Max_Logical_Reads - Memory pages read]					[bigint]			NOT NULL,
+		[Total_Elapsed_Time]							[numeric](18, 6)		NULL,
+		[Last_Elapsed_Time]							[numeric](18, 6)		NULL,
+		[Min_Elapsed_Time]							[numeric](18, 6)		NULL,
+		[Max_Elapsed_Time]							[numeric](18, 6)		NULL,
+		[Query_Hash]								[binary](8)			NOT NULL,
+		[Query_Plan_Hash]							[binary](8)			NOT NULL,
+		[Total_Rows - in all executions]					[bigint]			NOT NULL,
+		[Last_Rows]								[bigint]			NOT NULL,
+		[Min_Rows]								[bigint]			NOT NULL,
+		[Max_Rows]								[bigint]			NOT NULL,
+		[Filtered text]								[nvarchar](max)			NULL,
+		[Temp table]								[nvarchar](max)			NULL,
+		[Execution plan]							[xml]				NULL		
 	)
 as
 begin
@@ -109,10 +109,10 @@ begin
 	(select
 		sum(cast(qs.last_elapsed_time * 1.0 / power(10,6) as numeric(18,6))) over ()	[Procedure time]
 		
-		, dense_rank() over (order by qs.last_elapsed_time desc)						[rank-execution time]
-		, dense_rank() over (order by qs.last_logical_reads desc)						[rank-logical reads]
-		, dense_rank() over (order by qs.last_worker_time desc)							[rank-worker (CPU) time]
-		, dense_rank() over (order by qs.last_logical_writes desc)						[rank-logical write]
+		, dense_rank() over (order by qs.last_elapsed_time desc)			[rank-execution time]
+		, dense_rank() over (order by qs.last_logical_reads desc)			[rank-logical reads]
+		, dense_rank() over (order by qs.last_worker_time desc)				[rank-worker (CPU) time]
+		, dense_rank() over (order by qs.last_logical_writes desc)			[rank-logical write]
 
 		, qs.sql_handle																	[SQL_Handle]
 		, qs.statement_start_offset														[Statement_Start_Offset]
@@ -123,10 +123,10 @@ begin
 		, qs.last_execution_time														[Last_Execution_Time]
 		, qs.execution_count															[Execution_Count]
 
-		, cast(qs.total_worker_time * 1.0 / power(10,6) as numeric(18,6))				[Total_Worker_Time - CPU]										-- Total amount of CPU time that was consumed by executions of this plan since it was compiled (in seconds)
-		, cast(qs.last_worker_time * 1.0 / power(10,6) as numeric(18,6))				[Last_Worker_Time - CPU]										-- CPU time that was consumed the last time the plan was executed
-		, cast(qs.min_worker_time * 1.0 / power(10,6) as numeric(18,6))					[Min_Worker_Time - CPU]											-- CPU time that this plan has ever consumed during a single execution
-		, cast(qs.max_worker_time * 1.0 / power(10,6) as numeric(18,6))					[Max_Worker_Time - CPU]											-- Maximum CPU time that this plan has ever consumed during a single execution
+		, cast(qs.total_worker_time * 1.0 / power(10,6) as numeric(18,6))		[Total_Worker_Time - CPU]										-- Total amount of CPU time that was consumed by executions of this plan since it was compiled (in seconds)
+		, cast(qs.last_worker_time * 1.0 / power(10,6) as numeric(18,6))		[Last_Worker_Time - CPU]										-- CPU time that was consumed the last time the plan was executed
+		, cast(qs.min_worker_time * 1.0 / power(10,6) as numeric(18,6))			[Min_Worker_Time - CPU]											-- CPU time that this plan has ever consumed during a single execution
+		, cast(qs.max_worker_time * 1.0 / power(10,6) as numeric(18,6))			[Max_Worker_Time - CPU]											-- Maximum CPU time that this plan has ever consumed during a single execution
 		
 		, qs.total_physical_reads														[Total_Physical_Reads - Disk pages read]						-- Total number of physical reads performed by executions of this plan since it was compiled - (Will always be 0 querying a memory-optimized table)
 		, qs.last_physical_reads														[Last_Physical_Reads - Disk pages read]							-- Number of physical reads performed the last time the plan was executed
@@ -138,16 +138,16 @@ begin
 		, qs.min_logical_writes															[Min_Logical_Writes - Memory pages written]						-- Minimum number of logical writes that this plan has ever performed during a single execution
 		, qs.max_logical_writes															[Max_Logical_Writes - Memory pages written]						-- Maximum number of logical writes that this plan has ever performed during a single execution
 
-			-- logical reads are reads of 8k pages that came from memory or from disk
+		-- logical reads are reads of 8k pages that came from memory or from disk
 		, qs.total_logical_reads														[Total_Logical_Reads - Memory (buffer cache) pages read]		-- Total number of logical reads performed by executions of this plan since it was compiled - (Will always be 0 querying a memory-optimized table)
 		, qs.last_logical_reads															[Last_Logical_Reads - Memory pages read]						-- Number of logical reads performed the last time the plan was executed
 		, qs.min_logical_reads															[Min_Logical_Reads - Memory pages read]							-- Minimum number of logical reads that this plan has ever performed during a single execution
 		, qs.max_logical_reads															[Max_Logical_Reads - Memory pages read]							-- Maximum number of logical reads that this plan has ever performed during a single execution
 
-		, cast(qs.total_elapsed_time * 1.0 / power(10,6) as numeric(18,6))				[Total_Elapsed_Time]											-- Total elapsed time for completed executions of this plan (in seconds)
-		, cast(qs.last_elapsed_time * 1.0 / power(10,6) as numeric(18,6))				[Last_Elapsed_Time]												-- Elapsed time for the most recently completed execution of this plan
-		, cast(qs.min_elapsed_time * 1.0 / power(10,6) as numeric(18,6))				[Min_Elapsed_Time]												-- Minimum elapsed time for any completed execution of this plan
-		, cast(qs.max_elapsed_time * 1.0 / power(10,6) as numeric(18,6))				[Max_Elapsed_Time]												-- Maximum elapsed time for any completed execution of this plan
+		, cast(qs.total_elapsed_time * 1.0 / power(10,6) as numeric(18,6))		[Total_Elapsed_Time]											-- Total elapsed time for completed executions of this plan (in seconds)
+		, cast(qs.last_elapsed_time * 1.0 / power(10,6) as numeric(18,6))		[Last_Elapsed_Time]												-- Elapsed time for the most recently completed execution of this plan
+		, cast(qs.min_elapsed_time * 1.0 / power(10,6) as numeric(18,6))		[Min_Elapsed_Time]												-- Minimum elapsed time for any completed execution of this plan
+		, cast(qs.max_elapsed_time * 1.0 / power(10,6) as numeric(18,6))		[Max_Elapsed_Time]												-- Maximum elapsed time for any completed execution of this plan
 
 		, qs.query_hash																	[Query_Hash]													-- Binary hash value calculated on the query and used to identify queries with similar logic. You can use the query hash to determine the aggregate resource usage for queries that differ only by literal values.
 		, qs.query_plan_hash															[Query_Plan_Hash]												-- Binary hash value calculated on the query execution plan and used to identify similar query execution plans - (Will always be 0x000 when a natively compiled stored procedure queries a memory-optimized table)
@@ -163,7 +163,7 @@ begin
 					then datalength(st.text)
 				else
 					qs.statement_end_offset
-				end - qs.statement_start_offset) / 2 + 1)) as 							[Filtered text]
+				end - qs.statement_start_offset) / 2 + 1)) as 			[Filtered text]
 		, cast(qp.query_plan as xml) 													[Execution plan]
 		, qp.query_plan 																[Execution plan - Plan Explorer version]
 		, st.objectid																	[Object ID]
